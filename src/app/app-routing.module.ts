7e2ff019b83cec0715dafc3efe7bf664ba4fbe68
Sebakite272 <seba.gajardo@duocuc.cl>
2023-10-19 15:24:57 -0300

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+
 
 const routes: Routes = [
   {
@@ -13,6 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'alert',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
   },
   {
@@ -29,10 +32,12 @@ const routes: Routes = [
   },
   {
     path: 'reserva1',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./reserva1/reserva1.module').then( m => m.Reserva1PageModule)
   },
   {
     path: 'vehiculos-prop',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./vehiculos-prop/vehiculos-prop.module').then( m => m.VehiculosPropPageModule)
   },
   {
@@ -41,35 +46,43 @@ const routes: Routes = [
   },
   {
     path: 'verificar-disp',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./verificar-disp/verificar-disp.module').then( m => m.VerificarDispPageModule)
   },
   {
     path: 'no-dispo',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./no-dispo/no-dispo.module').then( m => m.NoDispoPageModule)
   },
   {
     path: 'si-dispo',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./si-dispo/si-dispo.module').then( m => m.SiDispoPageModule)
   },
 
   {
     path: 'confirmar-viaje',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./confirmar-viaje/confirmar-viaje.module').then( m => m.ConfirmarViajePageModule)
   },
   {
     path: 'viaje-exitoso',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./viaje-exitoso/viaje-exitoso.module').then( m => m.ViajeExitosoPageModule)
   },
   {
     path: 'cerrar-sesion',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./cerrar-sesion/cerrar-sesion.module').then( m => m.CerrarSesionPageModule)
   },
   {
     path: 'success-sc',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./success-sc/success-sc.module').then( m => m.SuccessScPageModule)
   },
   {
     path: 'reserva2',
+    canActivate: [AuthGuard],
     loadChildren: () => import('./reserva2/reserva2.module').then( m => m.Reserva2PageModule)
   },
   {
