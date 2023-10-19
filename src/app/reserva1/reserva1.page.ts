af1eb9e0c6e0a45977494196a11c01895eec5a33
Sebakite272 <seba.gajardo@duocuc.cl>
2023-10-18 22:29:57 -0300

    c

--- src/app/reserva1/reserva1.page.ts
+++ src/app/reserva1/reserva1.page.ts
@@ -11,6 +11,8 @@ export class Reserva1Page implements OnInit {
   constructor(private router: Router, private activatedRouter: ActivatedRoute, private authGuard: AuthGuard) { }
 
   public informacion = {
+    hora:"",
+    cant_personas:"",
     precio:""
   }
 
@@ -20,12 +22,14 @@ export class Reserva1Page implements OnInit {
     this.activatedRouter.queryParams.subscribe(() => {
       let state = this.router.getCurrentNavigation()?.extras.state;
       if (state) {
+        this.informacion.hora = state['informacion'].hora;
+        this.informacion.cant_personas = state['informacion'].cant_personas;
         this.informacion.precio = state['informacion'].precio;
         console.log(this.informacion);
       }
     })
   }
-  
+
   }
 
 
